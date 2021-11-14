c7c2151b26dc91907685acc8b67ebffc73045888
Philippe Merle <philippe.merle@inria.fr>
2021-11-14 14:57:41 +0100

    Fixed diagram generation script

--- generate_diagrams.sh
+++ generate_diagrams.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
-for format in [ png, svg]
+for format in png svg
 do
+  echo Generating TOSCA-Implementation-Landscape.${format}
   java -jar plantuml.jar -T${format} TOSCA-Implementation-Landscape.puml
 done
