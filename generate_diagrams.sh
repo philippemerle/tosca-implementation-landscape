a738547a828265662a0e93f723b1bbfa763924bc
Philippe Merle <philippe.merle@inria.fr>
2021-11-14 19:33:09 +0100

    Added header

--- generate_diagrams.sh
+++ generate_diagrams.sh
@@ -1,4 +1,15 @@
 #! /bin/sh
+######################################################################
+#
+# Copyright (c) 2021 Inria
+#
+# This software is distributed under the Apache License 2.0
+# the text of which is available at http://www.apache.org/licenses/LICENSE-2.0
+# or see the "LICENSE-2.0.txt" file for more details.
+#
+# Author: Philippe Merle <philippe.merle@inria.fr>
+#
+######################################################################
 
 for format in png svg
 do
